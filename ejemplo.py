d6c5582c04f9a38720179fbe28e1e8aadc1b9ab3
Oscar <wirdoing@gmail.com>
2019-09-06 19:44:22 -0500

    espacio borrado

--- ejemplo.py
+++ ejemplo.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-import unittest,re, os
+import unittest,re,os
 import scriptKeyboard
 class AppDynamicsJob(unittest.TestCase):
     def setUp(self):
