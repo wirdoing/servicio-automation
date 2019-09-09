50839cbc432235ccb0ee3f996d25a90d92042be8
Oscar <wirdoingqgmail.com>
2019-09-09 00:26:52 -0500

    add headless

--- ejemplo.py
+++ ejemplo.py
@@ -3,11 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.firefox.options import Options
 import unittest,re,os
 import scriptKeyboard
 class AppDynamicsJob(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        options = Options()
+        options.headless = True
+        self.driver = webdriver.Firefox(options=options)
         self.driver.implicitly_wait(30)
         self.verificationErrors = []
         self.accept_next_alert = True
